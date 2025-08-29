ef9d52f5b574156987b6f24a45324df3b33c9235
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2025-08-29 21:18:42 +0700

    Add server allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +7,6 @@ export default defineConfig({
   server: {
     port: 8080,
     host: 'localhost',
-  }
-})
+    allowedHosts: ['multi-pacman-game.westeurope.cloudapp.azure.com'],
+  },
+});
